7ec86f41862b72ddb7efb38db132260be6f42bf7
egleticia <leticiaegle@yahoo.com.br>
2023-05-06 14:04:48 -0300

    First Model-Controller-View - Departament - Part II

---
+++ WebSalesMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesMvc.Models;
+using System.Collections.Generic;
+
+namespace WebSalesMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
